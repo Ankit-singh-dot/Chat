17e6362b4360f56b5f0e21f63c9024a19ae360a0
Ankit <ankitrajdihara123@gmail.com>
2025-10-15 13:19:57 +0530

    conversation_schema

--- backend/models/Conversation.js
+++ backend/models/Conversation.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 const conversationSchema = new mongoose.Schema({
   participants: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+//   participant are actully stored in array and refeing to user 
   lastMessage: { type: mongoose.Schema.ObjectId, ref: "Messages" },
   unreadCount: { type: Number, default: 0 },
 });
 const Conversation = mongoose.model("Conversation", conversationSchema);
-export default conversationSchema;
+export default Conversation;
