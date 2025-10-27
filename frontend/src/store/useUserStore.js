3ea3e410359ecb59654044749086549aed5d4456
Ankit <ankitrajdihara123@gmail.com>
2025-10-28 05:20:15 +0530

    useUserStore_redux

---
+++ frontend/src/store/useUserStore.js
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+const useUserStore = create(
+  persist(
+    (set) => {
+      user: null;
+      isAuthenticated: null;
+      setUser: (userData) => set({ user: userData, isAuthenticated: true });
+      clearUser: () => set({ userData: null, isAuthenticated: false });
+    },
+    {
+      name: "user-storage",
+      getStorage: () => localStorage,
+    }
+  )
+);
