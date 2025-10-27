b592166c9ea7da00d48c982b87ac0844493e6eda
Ankit <ankitrajdihara123@gmail.com>
2025-10-28 04:02:38 +0530

    verify_otp

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -3,6 +3,7 @@ interface sendOtpPayLoad {
   phoneNumber: string;
   phoneSuffix: string;
   email: string;
+  otp: string;
 }
 export const sendOtp = async ({
   phoneNumber,
@@ -14,6 +15,25 @@ export const sendOtp = async ({
       phoneNumber,
       phoneSuffix,
       email,
+    });
+    return response.data;
+  } catch (error: any) {
+    throw error.response ? error.response.data : error.message;
+  }
+};
+
+export const verifyOtp = async ({
+  phoneNumber,
+  phoneSuffix,
+  email,
+  otp,
+}: sendOtpPayLoad) => {
+  try {
+    const response = await axiosInstance.post("/auth/verify-otp", {
+      phoneNumber,
+      phoneSuffix,
+      email,
+      otp,
     });
     return response.data;
   } catch (error: any) {
