2ac2aa2bd3dbcb07139b06c9492fa2971c5dcc07
Ankit <ankitrajdihara123@gmail.com>
2025-10-30 13:40:19 +0530

    login_half_done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,6 +8,8 @@ import useThemeStore from "../store/themeStore";
 import useUserStore from "../store/useUserStore";
 import useLoginStore from "../store/useLoginStore";
 import { motion } from "framer-motion";
+import { FaWhatsapp } from "react-icons/fa";
+
 const loginValidationSchema = yup
   .object()
   .shape({
@@ -93,6 +95,21 @@ const Login = () => {
   } = useForm({
     resolver: yupResolver(profileValidationSchema),
   });
+
+  const ProgressBar = () => {
+    return (
+      <div
+        className={`w-full ${
+          theme === "dark" ? "bg-gray-700" : "bg-white"
+        } rounded-full h-2.5 mb-6`}
+      >
+        <div
+          className="bg-green-500 h-2.5 rounded-full transition-all duration-500 ease-in-out"
+          style={{ width: `${(step / 3) * 100}` }}
+        ></div>
+      </div>
+    );
+  };
   return (
     <div
       className={`min-h-screen ${
@@ -107,9 +124,44 @@ const Login = () => {
         transition={{ duration: 0.5 }}
         className={`${
           theme === "dark" ? "bg-gray-800 text-white" : "bg-white"
-        } p-6 md:p-8 rounded-lg shadow-2xl  max-w-md relative z-10`}
+        } p-6 md:p-8 rounded-lg shadow-2xl w-full  max-w-md relative z-10`}
         // max-w-md => This box won’t grow wider than this limit even on big screens. 	Sets the maximum width of the box to a medium size
-      ></motion.div>
+      >
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{
+            duration: 0.2,
+            damping: 20,
+            stiffness: 250,
+            type: "spring",
+          }}
+          className="w-24 h-24 bg-green-500 rounded-full mx-auto mb-6 flex items-center justify-center"
+        >
+          <FaWhatsapp className="size-10" />
+        </motion.div>
+        <h1
+          className={`text-2xl font-bold text-center mb-6 ${
+            theme === "dark" ? "text-white" : "text-gray "
+          }`}
+        >
+          {" "}
+          WhatsApp login
+        </h1>
+        <ProgressBar />
+        {error && <p className="text-red-500 text-centre mb-4">{error}</p>}
+        {step === 1 && (
+          <form className="space-y-4">
+            <p
+              className={`text-center ${
+                theme === "dark" ? "text-white-300" : "text-white-700"
+              } mb-4`}
+            >
+              Enter your phone number to receive otp{" "}
+            </p>
+          </form>
+        )}
+      </motion.div>
     </div>
   );
 };
